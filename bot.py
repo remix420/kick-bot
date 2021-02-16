125dad3fdbde4aca1e4f35912e20c40b49afc8e9
remix <72325094+remixpy@users.noreply.github.com>
2021-02-16 11:42:28 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,20 +3,24 @@ import asyncio
 from discord.ext import tasks
 from discord.ext import commands
 
-
-
-
 token = ''
 
 
-
-
-
 prefix = '!'
-client = commands.Bot(command_prefix = prefix)
-client.remove_command("help")
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True) # Enable intents in discord devolper portal!!!
+client = commands.Bot(command_prefix = prefix, intents = intents)
+
+
+
+	
+
+
+
 
 count = 0
+
+
+
 
 @client.event
 async def on_member_join(member : discord.Member):
